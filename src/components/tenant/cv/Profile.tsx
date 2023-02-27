0ee4afe3491de049d717148ff32a008a09a414f6
Juan Pedro Armella <juanpedroarmella@gmail.com>
2023-02-27 17:38:48 -0300

    feat(cv): added profile text

--- src/components/tenant/cv/Profile.tsx
+++ src/components/tenant/cv/Profile.tsx
@@ -1,18 +1,15 @@
 import { Typography } from "@mui/material";
+import useTranslation from "next-translate/useTranslation";
 import dynamic from "next/dynamic";
 
 const Box = dynamic(() => import("@mui/material/Box"));
 
 const Profile: React.FC = () => {
+  const { t } = useTranslation("cv");
   return (
-    <Box display="grid" gap={2}>
-      <Typography variant="h3">Profile</Typography>
-      <Typography variant="body1">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
-        beatae neque rerum veritatis omnis obcaecati velit corrupti expedita ab
-        aliquid animi excepturi sequi odio magni pariatur, explicabo distinctio
-        cum quam?
-      </Typography>
+    <Box display="grid" gap={2} width='100%'>
+      <Typography variant="h3">{t("profile-title")}</Typography>
+      <Typography variant="body1">{t("profile-text")}</Typography>
     </Box>
   );
 };
