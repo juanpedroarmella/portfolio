80bfb8aa11990397c09f1812023c39cd2a1a7475
Juan Pedro Armella <juanpedroarmella@gmail.com>
2023-03-18 17:18:42 -0300

    feat(theme): replaced some hardcoded styles

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,5 +1,7 @@
 import useIsMobile from "@/hooks/useIsMobile";
 import styled from "@emotion/styled";
+import Box from "@mui/material/Box";
+import { Theme } from "@mui/material/styles";
 import dynamic from "next/dynamic";
 
 const Nav = dynamic(() => import("@/components/layout/header/Nav"));
@@ -10,19 +12,23 @@ const MobileHeader = dynamic(
   () => import("@/components/layout/header/MobileHeader")
 );
 
-const HeaderWrapper = styled("header")({
-  position: "fixed",
-  top: 0,
-  left: 0,
-  width: "100%",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  background: "white",
-  padding: "0.5rem 1rem",
-  boxShadow:
-    "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
-  zIndex: 1,
+const HeaderWrapper = styled(Box)(({ theme }: { theme?: Theme }) => {
+  return {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "90%",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    background: theme?.palette.background.default,
+    padding: "0.5rem 1rem",
+    boxShadow:
+      "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+    borderRadius: 10,
+    marginTop: "0.5rem",
+    zIndex: 1,
+  };
 });
 
 const Header = () => {
@@ -31,7 +37,7 @@ const Header = () => {
   if (isMobile) return <MobileHeader />;
 
   return (
-    <HeaderWrapper>
+    <HeaderWrapper component="header" mx={"5%"}>
       <Nav />
       <Settings />
     </HeaderWrapper>
--- src/components/layout/header/MobileHeader.tsx
+++ src/components/layout/header/MobileHeader.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Theme } from "@mui/material";
 import dynamic from "next/dynamic";
 
 const Settings = dynamic(() => import("@/components/settings/Settings"));
@@ -8,19 +9,21 @@ const IconButton = dynamic(() => import("@mui/material/IconButton"));
 const MenuIcon = dynamic(() => import("@mui/icons-material/Menu"));
 const Drawer = dynamic(() => import("@/components/atoms/Drawer"));
 
-const MobileHeaderWrapper = styled(FlexBox)({
-  position: "fixed",
-  top: 0,
-  width: "100%",
-  left: 0,
-  display: "flex",
-  justifyContent: "flex-end",
-  alignItems: "center",
-  background: "white",
-  padding: "0.5rem 1rem",
-  boxShadow:
-    "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
-  zIndex: 1,
+const MobileHeaderWrapper = styled(FlexBox)(({ theme }: { theme?: Theme }) => {
+  return {
+    position: "fixed",
+    top: 0,
+    width: "100%",
+    left: 0,
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    background: theme?.palette.background.default,
+    padding: "0.5rem 1rem",
+    boxShadow:
+      "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+    zIndex: 1,
+  };
 });
 
 const ElementsWrapper = styled(FlexBox)({
